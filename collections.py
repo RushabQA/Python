3d724163a6be207e2bdebcfbf9c7b3cdbff5b165
RushabQA <65768895+RushabQA@users.noreply.github.com>
2020-06-02 16:26:18 +0100

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,15 @@
+cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
+cool_sheep = ["Baaaart", "Baaaarnaby"]
+cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"]
+
+cool_animals = [cool_cows, cool_sheep, cool_pigs]
+
+print(cool_animals[2][2])
+print(cool_animals[0][1])
+
+
+books = {"Harry Potter":"J.K. Rowling", "The Hobbit":"J.R.R. Tolkien", "Charlie and the Chocolate Factory":"Roald Dahl"}
+
+print(books["Harry Potter"])
+print(books["The Hobbit"])
+
